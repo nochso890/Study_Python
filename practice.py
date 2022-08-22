2280c1b1f002e3cd0e615aabe7e285f980022bc1
changsong-noh-switchwon <nochso@switchwon.com>
2022-08-22 19:28:30 +0900

    Initial commit

---
+++ practice.py
@@ -0,0 +1,24 @@
+# 자료형
+
+# 숫자
+print(5)
+print(-10)
+print(5-10)
+print(3.10)
+print(3.14 * 3)
+
+# 문자
+print("큰따옴표")
+print('작은따옴표')
+print("   공백")
+print(" " * 4 +"공백")
+
+
+#참 / 거짓
+print(6>7)
+print(5==5)
+print(6 <= 8)
+print("ABC" == "abc")
+print("ABC" == "ABC")
+
+print(not (1 <= 2))
